0792da1ee630b3849a08287c93f1fa3e6fcebed2
Nohan Gustavo <81246430+NouanGust@users.noreply.github.com>
2025-05-08 13:27:38 -0300

    Arquivos da aula 08.05

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const URL = 'https://catfact.ninja/fact'
+const URL_FATOS = 'https://catfact.ninja/fact'
+const URL_FOTO = 'https://api.thecatapi.com/v1/images/search'
 
 let $fatos_p = document.getElementById('fatos')
 let $btn_pegar_fatos = document.getElementById('btn_pegar_fatos')
+let $foto = document.getElementById('foto')
 
 
 
 $btn_pegar_fatos.addEventListener('click', async () => {
-    let response = await(fetch(URL))
-    let obj = await(response.json())
+    $fatos_p.innerText = 'Carregando...'
+    $foto.src = 'imagens/loadingCat.gif'
 
-    $fatos_p.innerText = obj.fact.trim()
+    // Requisição dos fatos
+    let fato_response = await(fetch(URL_FATOS))
+    let fato_obj = await(fato_response.json())
+
+    // Requisição das fotos
+    let foto_response = await(fetch(URL_FOTO))
+    let foto_obj = await(foto_response.json())
+
+    console.log(foto_response)
+    console.log(foto_obj)
+    console.log(foto_obj[0].url)
+    console.log(foto_obj[0].width)
+    console.log(foto_obj[0].height)
+    console.log(fato_obj)
+    $fatos_p.innerText = fato_obj.fact.trim()
+    $foto.src = foto_obj[0].url
 })
 
-// async function PegaFato() {
-//     let response = await fetch(URL)
-//     let obj = await response.json()
-//     let fato = obj.fact.trim()
-//     $fatos_p.innerText = fato
-
-// }
